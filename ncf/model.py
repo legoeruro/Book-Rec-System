db9e5d504e5e6d5b9fcbc0c2dea0bf6765dd4301
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2024-04-25 15:16:46 -0400

    NCF updates

--- ncf/model.py
+++ ncf/model.py
@@ -17,14 +17,15 @@ class NCFNet(nn.Module):
     Based on: https://arxiv.org/abs/1708.05031
     """
 
-    def __init__(self, num_items, num_users, embedding_dim, num_mlp_layers):
+    def __init__(self, num_items, num_users, embedding_dim, mlp_layer_sizes=(256, 128, 64)):
         super(NCFNet, self).__init__()
 
         self.ilv_embedding = nn.Embedding(num_items, embedding_dim)
         self.ulv_embedding = nn.Embedding(num_users, embedding_dim)
 
         mlp_input_size = embedding_dim * 2  # Concatenation of embeddings fed into the mlp layers
-        self.mlp_layers = torch_ops.MLP(mlp_input_size, num_mlp_layers)
+        self.mlp_layers = torch_ops.MLP(mlp_input_size, list(mlp_layer_sizes))
+        self.mlp_layers.append(nn.Linear(mlp_layer_sizes[-1], 1))
 
     def forward(self, user_vec, item_vec):
         concat_ed = torch.concat((self.ilv_embedding(item_vec), self.ulv_embedding(user_vec)), dim=1)
