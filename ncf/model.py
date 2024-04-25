a601b161add8fac8350f7d8a7a94dfe126943b5f
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2024-04-25 14:51:58 -0400

    Neural Collaborative Filtering Model Base Added Scikit-learn and CUDA Pytorch to poetry config

---
+++ ncf/model.py
@@ -0,0 +1,32 @@
+"""
+Neural Collaborative Filtering Model
+Based on https://arxiv.org/abs/1708.05031
+
+Author: Ivan Klevanski
+
+"""
+
+import torch
+import torch.nn as nn
+import torchvision.ops as torch_ops
+
+
+class NCFNet(nn.Module):
+    """
+    Neural Collaborative Filtering Model\n
+    Based on: https://arxiv.org/abs/1708.05031
+    """
+
+    def __init__(self, num_items, num_users, embedding_dim, num_mlp_layers):
+        super(NCFNet, self).__init__()
+
+        self.ilv_embedding = nn.Embedding(num_items, embedding_dim)
+        self.ulv_embedding = nn.Embedding(num_users, embedding_dim)
+
+        mlp_input_size = embedding_dim * 2  # Concatenation of embeddings fed into the mlp layers
+        self.mlp_layers = torch_ops.MLP(mlp_input_size, num_mlp_layers)
+
+    def forward(self, user_vec, item_vec):
+        concat_ed = torch.concat((self.ilv_embedding(item_vec), self.ulv_embedding(user_vec)), dim=1)
+        output = torch.sigmoid(self.mlp_layers(concat_ed)).view(-1)
+        return output
